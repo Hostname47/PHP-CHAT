22c924dae6fc30b01f706564800fcabe771812a7
HOSTNAME47 <mouadstev1@gmail.com>
2020-12-15 22:34:02 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>V01D47</title>
+</head>
+<body>
+    <h1>THIS IS VOID47 UNDERGROUND CHAT APPLICATION</h1>
+</body>
+</html>
