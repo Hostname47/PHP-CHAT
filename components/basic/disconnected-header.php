cb524fce3f3e34ee6df46b8f844ddcc544c4c864
Mouad <mouadstev1@gmail.com>
2020-12-16 17:15:27 +0100

    Add disconnected header UI

---
+++ components/basic/disconnected-header.php
@@ -0,0 +1,28 @@
+<header>
+    <div>
+        <a href="#"><img src="assets/images/logos/large.png" alt="logo" class="wide-logo"></a>
+    </div>
+    <div class="inline-logo-separator">〡</div>
+    <div id="menu-login-credentials-container">
+        <div style="margin: 0 12px"></div>
+        <div class="col-flex">
+            <label for="email-or-phone" class="small-label">Email or Phone</label>
+            <input type="text" id="email-or-phone" class="text-input medium-text-input" form="login-form" placeholder="Email">
+            <div class="row-v-flex">
+                <input type="checkbox" id="keep-me-connected" form="login-form" checked>
+                <label class="small-label" for="keep-me-connected">Kepp me connected</label>
+            </div>
+        </div>
+        <div style="margin: 0 4px"></div>
+        <div class="col-flex">
+            <label for="email-or-phone" class="small-label">Password</label>
+            <input type="password" id="password" class="text-input medium-text-input" form="login-form" placeholder="Password">
+            <a href="" class="link">Forgotten your passowrd?</a>
+        </div>
+        <div style="margin: 0 4px"></div>
+        <form action="" class="flex-form" id="login-form">
+            <input type="submit" id="connect" value="Login" class="button-style-1">
+        </form>
+        <div style="margin: 0 12px 0 4px"></div>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -4,8 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>V01D47</title>
+    <link rel='shortcut icon' type='image/x-icon' href='assets/images/favicons/favicon.ico' />
+    <link rel="stylesheet" href="styles/global.css">
+    <link rel="stylesheet" href="styles/header.css">
 </head>
 <body>
-    <h1>THIS IS VOID47 UNDERGROUND CHAT APPLICATION</h1>
+    <!-- Here before including the header, we need to make sure if user is already connected. If so
+         we need to add connected header, otherwise disconnected header wil be shown
+    -->
+    <?php include "components/basic/disconnected-header.php" ?>
+
+
 </body>
 </html>
