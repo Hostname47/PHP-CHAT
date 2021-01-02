7721b50cd9a17900b9dd99a6ae8230ed9d487e5e
HOSTNAME47 <mouadstev1@gmail.com>
2021-01-02 23:35:22 +0100

    add new features to profile page including picture and cover viewers and Edit profile UI

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,2 +1,89 @@
 
-$("#profile-picture").css("borderColor", $("body").css("backgroundColor"));
+$(".profile-picture").css("borderColor", $("#first-section").css("backgroundColor"));
+
+
+$(".viewer").css({
+    width: $("body").width(),
+    height: $("body").height()
+})
+
+$(".close-viewer").click(function() {
+    $(".viewer").css("display", "none");
+
+    return false;
+})
+
+$("#edit-profile-button").on({
+    click: function() {
+        let viewer = $(this).parent().find(".viewer");
+        viewer.css("display", "block");
+        return false;
+    }
+});
+
+$(".cover-photo").on({
+    click: function() {
+        let viewer = $(this).parent().parent().find(".viewer");
+        viewer.find(".profile-cover-picture-preview").attr("src", $(".cover-photo").attr("src"));
+        viewer.css("display", "block");
+        return false;
+    }
+});
+
+$(".profile-picture").on({
+    mouseenter: function() {
+        $(".shadow-profile-picture").css("opacity", "0.2");
+    },
+    mouseleave: function() {
+        $(".shadow-profile-picture").css("opacity", "0");
+    },
+    click: function() {
+        let viewer = $(this).parent().parent().find(".viewer");
+        viewer.css("display", "block");
+        viewer.find(".profile-picture-preview").attr("src", $(".profile-picture").attr("src"));
+        return false;
+    }
+});
+
+$("#cover-changer-container").on({
+    mouseenter: function() {
+        $(this).find("#cover-changer-shadow, .change-image-icon").css("display", "block");
+    },
+    mouseleave: function() {
+        $(this).find("#cover-changer-shadow, .change-image-icon").css("display", "none");
+    }
+});
+
+$("#picture-changer-container").on({
+    mouseenter: function() {
+        $(this).find(".former-picture-shadow, .change-image-icon").css("display", "block");
+    },
+    mouseleave: function() {
+        $(this).find(".former-picture-shadow, .change-image-icon").css("display", "none");
+    }
+});
+
+$(".profile-picture-preview, .profile-cover-picture-preview, #edit-profile-container").click(function(event) {
+    event.stopPropagation();
+})
+
+$(".viewer").click(function() {
+    $(this).css("display", "none");
+})
+
+$(".user-info-section-link").on( {
+    mouseenter: function() {
+        $(this).find("div p").css("textDecoration", "underline");
+    },
+    mouseleave: function() {
+        $(this).find("div p").css("textDecoration", "none");
+    }
+}
+);
+
+$(".user-info-square-shape").css("height", $(".user-info-square-shape").css("width"))
+
+$("#change-cover-button").click(function() {
+    
+    return false;
+});
--- profile.php
+++ profile.php
@@ -39,10 +39,31 @@ if(isset($_POST["logout"])) {
 <body>
     <?php include_once "components/basic/header.php"; ?>
     <main>
-        <section id="firt-section">
+        <section id="first-section">
             <div class="relative flex-column">
-                <img src="" id="cover-photo" alt="">
-                <img src="assets/images/logos/logo512.png" id="profile-picture" alt="">
+                <div>
+                    <div id="cover-container">
+                        <img src="assets/images/programming.png" class="cover-photo" alt="">
+                    </div>
+                    <div class="viewer">
+                        <div class="relative">
+                            <a href="" class="close-viewer-style close-viewer"></a>
+                        </div>
+                        <img src="" class="profile-cover-picture-preview" alt="User's name cover picture">
+                    </div>
+                </div>
+                <div id="profile-picture-container">
+                    <div class="relative" style="border-radius: 50%">
+                        <img src="assets/images/read.png" class="profile-picture" alt="">
+                        <img src="" class="profile-picture shadow-profile-picture absolute" alt="">
+                    </div>
+                    <div class="viewer">
+                        <div class="relative">
+                            <a href="" class="close-viewer-style close-viewer"></a>
+                        </div>
+                        <img src="" class="profile-picture-preview" alt="User's name Profile picture">
+                    </div>
+                </div>
             </div>
             <div id="name-and-username-container">
                 <h1 class="title-style-3"><?php echo $user->getPropertyValue("firstname") . " " . $user->getPropertyValue("lastname"); ?></h1>
@@ -57,23 +78,122 @@ if(isset($_POST["logout"])) {
                     <a href="" class="profile-menu-item">Comments</a>
                 </div>
                 <div>
-                    <a href="" class="button-style-2">Edit profile</a>
+                    <a href="" class="button-style-2" id="edit-profile-button">Edit profile</a>
+                    <div class="viewer" style="display: block">
+                        <div id="edit-profile-container">
+                            <div class="flex-space" id="edit-profile-header">
+                                <a href="" class="close-style-1 close-viewer"></a>
+                                <h2 class="title-style-5 black">Edit profile</h2>
+                                <a href="" class="button-style-3">Save</a>
+                            </div>
+                            <div id="picture-and-cover-container">
+                                <a href="" id="change-cover-button">
+                                    <div id="cover-changer-container" class="relative">
+                                        <img src="assets/images/icons/change-image.png" class="absolute image-size-1 change-image-icon" alt="">
+                                        <img src="" id="cover-changer-shadow" style="z-index: 0" class="absolute" alt="">
+                                        <img src="assets/images/programming.png" id="cover-changer-dim" alt="">
+                                    </div>
+                                </a>
+                                <div class="relative flex-justify">
+                                    <a href="" id="change-picture-button" class="absolute">
+                                        <div id="picture-changer-container" class="relative">
+                                            <img src="assets/images/read.png" class="former-picture-dim" alt="">
+                                            <img src="assets/images/icons/change-image.png" class="absolute image-size-1 change-image-icon" alt="">
+                                            <img src="" class="former-picture-dim former-picture-shadow absolute" style="z-index: 0" alt="">
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div id="textual-data-edit">
+                                <div class="field-style-1">
+                                    <label for="display-name" class="label-style-1">Display name</label>
+                                    <input type="text" class="input-style-1" name="display-name">
+                                </div>
+                                <div class="field-style-1">
+                                    <label for="bio" class="label-style-1">Bio</label>
+                                    <textarea type="text" class="textarea-style-1" placeholder="Add your bio.." name="bio"></textarea>
+                                </div>
+                                <div class="field-style-1">
+                                    <label for="private" class="label-style-1">Private account</label>
+                                    <input type="text" class="input-style-1" name="display-name">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
         <section id="second-section">
             <div>
-                <div class="user-info-section">
-                    
+                <div class="user-info-section row-v-flex">
+                    <a href="" class="user-info-section-link">
+                        <div>
+                            <h2 class="title-style-4">2.9K</h2>
+                            <p class="regular-text-style-2">Posts</p>
+                        </div>
+                    </a>
+                    <a href="" class="user-info-section-link">
+                        <div>
+                            <h2 class="title-style-4">10</h2>
+                            <p class="regular-text-style-2">Followers</p>
+                        </div>
+                    </a>
+                    <a href="" class="user-info-section-link">
+                        <div>
+                            <h2 class="title-style-4">156</h2>
+                            <p class="regular-text-style-2">Following</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="user-info-section">
-                    
+                    <h2 class="title-style-4">About</h2>
+                    <p class="calendar-icon regular-text-style-2">Member since <!-- Join date should be fetched from db -->March 2020</p>
                 </div>
                 <div class="user-info-section">
-                    
+                    <div class="flex-space">
+                        <h2 class="title-style-4">Media</h2>
+                        <a href="" class="link-style-1">Show all</a>
+                    </div>
+                    <div>
+                        <div class="user-info-square-shapes-container">
+                            <div class="user-info-square-shape">
+                                
+                            </div>
+                            <div class="user-info-square-shape">
+                                
+                            </div>
+                            <div class="user-info-square-shape">
+                                
+                            </div>
+                            <div class="user-info-square-shape">
+                                
+                            </div>
+                        </div>
+                        <div class="user-info-square-shapes-container">
+                            <div class="user-info-square-shape">
+                                
+                            </div>
+                            <div class="user-info-square-shape">
+                                
+                            </div>
+                            <div class="user-info-square-shape">
+                                
+                            </div>
+                            <div class="user-info-square-shape">
+                                
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
             <div id="posts-container">
+                <div class="post-item1">
+
+                </div>    
+                <div class="post-item">
+
+                </div>
                 <div class="post-item">
 
                 </div>
